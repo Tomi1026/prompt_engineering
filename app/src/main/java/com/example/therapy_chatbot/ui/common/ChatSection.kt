01c481c9f3fa51daff7a730acfe119cccfe70efe
ENDHV <v.gerzson@gmail.com>
2024-05-15 21:21:33 +0200

    Update ChatSection.kt

--- app/src/main/java/com/example/therapy_chatbot/ui/common/ChatSection.kt
+++ app/src/main/java/com/example/therapy_chatbot/ui/common/ChatSection.kt
@@ -1,71 +1,55 @@
 package com.example.therapy_chatbot.ui.common
 
 import android.os.Build
-import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Send
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.therapy_chatbot.MainViewModel
 import com.example.therapy_chatbot.data.Message
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.time.Instant
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-
-
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun WindowContent(messages: List<Message>, viewModel: MainViewModel) {
+    val listState = rememberLazyListState()
+
+    LaunchedEffect(messages.size) {
+        listState.animateScrollToItem(messages.size - 1)
+    }
+
     Column(
         modifier = Modifier.fillMaxSize()
-    ){
-        LazyColumn (
+    ) {
+        LazyColumn(
+            state = listState,
             modifier = Modifier
                 .fillMaxWidth()
-                .wrapContentHeight()
-                .padding(16.dp)
-                .weight(7f),
+                .weight(1f)
+                .padding(16.dp),
             reverseLayout = false,
-        ){
-            items(messages.size){
-                val message = messages[it]
-                MessageCard(
-                    message = message
-                )
-                Spacer(modifier = Modifier.padding(8.dp))
-
+        ) {
+            items(messages.size) { index ->
+                val message = messages[index]
+                MessageCard(message = message)
+                Spacer(modifier = Modifier.height(8.dp))
             }
         }
-        MessageInput(viewModel, Modifier.weight(1f))
+        MessageInput(viewModel)
     }
 }
 
@@ -82,9 +66,7 @@ fun MessageCard(message: Message) {
                     shape = RoundedCornerShape(8.dp)
                 )
                 .padding(16.dp, 9.dp)
-
-        )
-        {
+        ) {
             Text(
                 text = message.content,
                 color = if (message.senderIsMe) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.onSecondary
@@ -95,55 +77,63 @@ fun MessageCard(message: Message) {
             color = if (message.senderIsMe) MaterialTheme.colorScheme.tertiary else MaterialTheme.colorScheme.tertiary,
         )
     }
-
 }
 
 @RequiresApi(Build.VERSION_CODES.O)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MessageInput(viewModel: MainViewModel, modifier: Modifier) {
+fun MessageInput(viewModel: MainViewModel) {
     var text by remember { mutableStateOf("") }
-    var response by remember { mutableStateOf("") }
+    var isLoading by remember { mutableStateOf(false) }
+    val coroutineScope = rememberCoroutineScope()
+    val focusManager = LocalFocusManager.current
 
-    Row (
-        modifier = modifier
+    Row(
+        modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp),
         verticalAlignment = Alignment.CenterVertically
-    )
-
-    {
+    ) {
         OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier.weight(1f),
             value = text,
             onValueChange = { text = it },
             label = { Text("Message") },
             maxLines = 4,
             trailingIcon = {
-                Icon(
-                    imageVector = Icons.Default.Send,
-                    contentDescription = "Send",
-                    modifier = Modifier.clickable {
-                        val temptext = text
-                        viewModel.messages.value += Message(true, temptext, time = LocalDateTime.now().format(
-                            DateTimeFormatter.ofPattern("HH:mm")))
-                        text = ""
-                        GlobalScope.launch(Dispatchers.IO) {
-                            val answer = viewModel.sendMessageToAssistant(temptext)
-                            withContext(Dispatchers.Main) {
-                                // Update UI with response
-                                viewModel.messages.value += Message(false, answer, time = LocalDateTime.now().format(
-                                    DateTimeFormatter.ofPattern("HH:mm")))
-                            }
-                        }
+                Box(modifier = Modifier.clickable {
+                    val tempText = text
+                    viewModel.messages.value += Message(
+                        true,
+                        tempText,
+                        time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm"))
+                    )
+                    text = ""
+                    isLoading = true
+                    focusManager.clearFocus() // Clear focus to hide the keyboard
+                    coroutineScope.launch {
+                        val answer = viewModel.sendMessageToAssistant(tempText)
+                        isLoading = false
+                        viewModel.messages.value += Message(
+                            false,
+                            answer,
+                            time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm"))
+                        )
                     }
-                )
+                }) {
+                    if (isLoading) {
+                        CircularProgressIndicator(modifier = Modifier.size(24.dp))
+                    } else {
+                        Icon(
+                            imageVector = Icons.Default.Send,
+                            contentDescription = "Send",
+                            modifier = Modifier.size(24.dp)
+                        )
+                    }
+                }
             }
         )
     }
 }
-
-
-
 
 
